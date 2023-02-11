dfe53ddfdfc6e7b119164977ec3d3fbd7e0f6ce9
DinhNhan <trandinhnhan9958@gmail.com>
2023-02-11 23:18:57 +0700

    add upload image ui

--- src/module/post/PostAddNew.js
+++ src/module/post/PostAddNew.js
@@ -6,9 +6,15 @@ import Label from "../../components/label/Label";
 import Input from "../../components/input/Input";
 import Button from "../../components/button/Button";
 import Radio from "../../components/checkbox/Radio";
-import Dropdown from "../../components/dropdown/Dropdown";
 import slugify from "slugify";
 import { postStatus } from "../../utils/constants";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { ImageUpload } from "../../components/image";
 
 const PostAddNewStyles = styled.div``;
 
@@ -26,9 +32,47 @@ const PostAddNew = () => {
   const watchCategory = watch("category");
   const addPostHandle = async (values) => {
     const cloneValues = { ...values };
-    console.log(cloneValues); // check xem giá trị gồm những gì để đưa vào db
+    // console.log(cloneValues); // check xem giá trị gồm những gì để đưa vào db
     cloneValues.slug = slugify(values.slug || values.title);
     cloneValues.status = Number(values.status);
+    // handleUploadImage(cloneValues.image);
+  };
+  const handleUploadImage = (file) => {
+    const storage = getStorage();
+    const storageRef = ref(storage, "images/" + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            console.log("Nothing");
+        }
+      },
+      (error) => {
+        console.log("error");
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+  };
+  const onSelectImage = (e) => {
+    // console.log(e.target.files);
+    const file = e.target.files[0];
+    if (!file) return;
+    setValue("image", file);
   };
   return (
     <PostAddNewStyles>
@@ -54,6 +98,10 @@ const PostAddNew = () => {
           </Field>
         </div>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
+          <Field>
+            <ImageUpload>Image</ImageUpload>
+            <input type="file" name="image" onChange={onSelectImage} />
+          </Field>
           <Field>
             <Label>Status</Label>
             <div className="flex items-center gap-x-5">
@@ -83,22 +131,12 @@ const PostAddNew = () => {
               </Radio>
             </div>
           </Field>
-          <Field>
-            <Label>Author</Label>
-            <Input control={control} placeholder="Find the author"></Input>
-          </Field>
         </div>
         <div className="grid grid-cols-2 gap-x-10 mb-10">
           <Field>
             <Label>Category</Label>
-            <Dropdown>
-              <Dropdown.Option>Knowledge</Dropdown.Option>
-              <Dropdown.Option>Blockchain</Dropdown.Option>
-              <Dropdown.Option>Setup</Dropdown.Option>
-              <Dropdown.Option>Nature</Dropdown.Option>
-              <Dropdown.Option>Developer</Dropdown.Option>
-            </Dropdown>
           </Field>
+          <Field></Field>
         </div>
         <Button type="submit" className="mx-auto">
           Thêm mới
