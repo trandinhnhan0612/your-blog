d5169d2743e3e5d99327093869a4feba4bd72d61
trandinhnhan0612 <trandinhnhan9958@gmail.com>
2023-04-11 14:09:23 +0700

    deploy project

--- src/firebase-data/firebase-config.js
+++ src/firebase-data/firebase-config.js
@@ -8,6 +8,14 @@ const firebaseConfig = {
   storageBucket: "your-blog-b8b35.appspot.com",
   messagingSenderId: "741399550345",
   appId: "1:741399550345:web:57c4c0369a642b9fea2958",
+  // apiKey: "AIzaSyAF-wUj26CZ8NDm9R380IAocmeNxdR_5jQ",
+  // authDomain: "your-blog-b8b35.firebaseapp.com",
+  // databaseURL:
+  //   "https://your-blog-b8b35-default-rtdb.asia-southeast1.firebasedatabase.app",
+  // projectId: "your-blog-b8b35",
+  // storageBucket: "your-blog-b8b35.appspot.com",
+  // messagingSenderId: "741399550345",
+  // appId: "1:741399550345:web:57c4c0369a642b9fea2958",
 };
 
 const app = initializeApp(firebaseConfig);
